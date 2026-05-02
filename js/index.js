42e23b562c1e0502d1871c54907190b54a0a216e
engy mohamed <mohamedengy319@gmail.com>
2026-05-02 20:05:13 +0300

    upload the design

---
+++ js/index.js
@@ -0,0 +1,103 @@
+var ProductNameInput = document.getElementById("ProductName");
+var ProductPriceInput = document.getElementById("ProductPrice");
+var ProductcategoryInput = document.getElementById("ProductCategory");
+var ProductDescriptionInput = document.getElementById("ProductDesciption");
+var ProductImageInput = document.getElementById("Productimage");
+var searchInput = document.getElementById("search");
+var addbtn = document.getElementById("addBtn");
+var updatebtn = document.getElementById("updateBtn");
+var currentIndex = 0;
+
+var productList = [];
+if (localStorage.getItem("productContainer") !== null) {
+  productList = JSON.parse(localStorage.getItem("productContainer"));
+  displayProduct();
+}
+
+function addProduct() {
+  var product = {
+    name: ProductNameInput.value.trim(),
+    price: ProductPriceInput.value,
+    category: ProductcategoryInput.value.trim(),
+    description: ProductDescriptionInput.value.trim(),
+    image: ProductImageInput.files[0]
+      ? `imgs/${ProductImageInput.files[0]?.name}`
+      : "imgs/img.jpg",
+  };
+
+  productList.push(product);
+  localStorage.setItem("productContainer", JSON.stringify(productList));
+  clearForm();
+  displayProduct();
+}
+
+function clearForm() {
+  ProductNameInput.value = null;
+  ProductPriceInput.value = null;
+  ProductcategoryInput.value = null;
+  ProductDescriptionInput.value = null;
+  ProductImageInput.value = null;
+}
+
+function displayProduct() {
+  var serchValue = searchInput.value;
+  var box = "";
+  for (let i = 0; i < productList.length; i++) {
+    if (productList[i].name.toLowerCase().includes(serchValue.toLowerCase())) {
+      box += `
+        <div class="col-md-3">
+            <div class="card mb-3">
+                  <img src="${productList[i].image}" class="card-img-top" alt="${productList[i].name}" />
+                  <div class="card-body">
+                    <h4 class="card-title">${productList[i].name.split(" ", 1).join(" ")}</h4>
+                    <h6 class="card-text">${productList[i].price}</h6>
+                    <h6 class="card-text">${productList[i].category.split(" ", 1).join(" ")}</h6>
+                    <h6 class="card-text">${productList[i].description.split(" ", 1).join(" ")}</h6>
+                  </div>
+                  <div class="card-footer text-center">
+                    <button onclick="deleteProduct(${i})" class="btn btn-outline-danger btn-sm my-3"> <i class="fa-solid fa-trash"></i> Delete</button>
+                    <button onclick="setProduct(${i})" class="btn btn-outline-warning btn-sm"><i class="fa-solid fa-edit"></i> update</button>
+                  </div>
+                </div>
+          </div>
+        `;
+    }
+  }
+  document.getElementById("rowData").innerHTML = box;
+}
+
+function deleteProduct(i) {
+  productList.splice(i, 1);
+  localStorage.setItem("productContainer", JSON.stringify(productList));
+  displayProduct();
+}
+
+function setProduct(i) {
+  currentIndex = i;
+  ProductNameInput.value = productList[i].name;
+  ProductPriceInput.value = productList[i].price;
+  ProductcategoryInput.value = productList[i].category;
+  ProductDescriptionInput.value = productList[i].description;
+
+  addbtn.classList.add("d-none");
+  updatebtn.classList.remove("d-none");
+}
+
+function updateProduct() {
+  var product = {
+    name: ProductNameInput.value,
+    price: ProductPriceInput.value,
+    category: ProductcategoryInput.value,
+    description: ProductDescriptionInput.value,
+    image: ProductImageInput.files[0]
+      ? `imgs/${ProductImageInput.files[0]?.name}`
+      : "imgs/img.jpg",
+  };
+
+  productList.splice(currentIndex, 1, product);
+  localStorage.setItem("productContainer", JSON.stringify(productList));
+  displayProduct();
+  clearForm();
+  addbtn.classList.remove("d-none");
+  updatebtn.classList.add("d-none");
+}
